158465c98765f30448a4de397f85fc9cea239c2c
Wojciech Frycz <wojciechfrycz@gmail.com>
2019-03-01 13:45:27 +0100

    Refactored view tests

--- apps/secure_url/tests/tests_secured_entity_create_view.py
+++ apps/secure_url/tests/tests_secured_entity_create_view.py
@@ -1,40 +1,11 @@
-import tempfile
-
-from PIL import Image
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls.base import reverse
 from rest_framework import status
 
+from .tests_base_view import BaseViewTest
 from ..constants import SecuredEntityTypes
 from ..models import SecuredEntity
 
 
-class SecuredEntityCreateViewTest(TestCase):
-    def setUp(self):
-        self.create_url = reverse('secure_url:secured-entity-create-view')
-        self.login_url = reverse('accounts-login')
-
-        self.username = 'test'
-        self.password = '123qweasd'
-
-        self.user = get_user_model().objects.create_user(username=self.username, password=self.password)
-
-        self.data_with_url = {
-            'url': 'https://www.facebook.com/'
-        }
-
-    def _get_tmp_file(self):
-        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
-
-        image = Image.new('RGB', (100, 100))
-        image.save(tmp_file)
-
-        return tmp_file
-
-    def _login_user(self):
-        self.client.login(username=self.username, password=self.password)
-
+class SecuredEntityCreateViewTest(BaseViewTest):
     def test_create_secured_entity_has_to_be_authenticated(self):
         response = self.client.get(self.create_url)
 
